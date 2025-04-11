5b8e07b8cb10b3e6e3436e7c1fffb4725c8e33d3
p-acid <qkrtkstjd01@gmail.com>
2025-04-11 16:55:17 +0900

    chore: add HMR settings

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,7 +1,15 @@
 import { app, BrowserWindow } from "electron";
 import { join } from "path";
+import { isDev } from "./util.js";
 
 app.on("ready", () => {
   const mainWindow = new BrowserWindow({});
-  mainWindow.loadFile(join(app.getAppPath(), "/dist-react/index.html"));
+
+  const isDevelopment = isDev();
+
+  if (isDevelopment) {
+    mainWindow.loadURL("http://localhost:5123");
+  } else {
+    mainWindow.loadFile(join(app.getAppPath(), "/dist-react/index.html"));
+  }
 });
